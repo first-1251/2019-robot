b4a1c0eec85b97364ebdab3f63ce5d0aaa86b71a
Chris Russell <crussell@sonnysdirect.com>
2019-03-14 08:54:33 -0400

    expanded motor test

--- src/main/java/org/team1251/frc/robot/commands/test/MotorTest.java
+++ src/main/java/org/team1251/frc/robot/commands/test/MotorTest.java
@@ -1,6 +1,10 @@
 package org.team1251.frc.robot.commands.test;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+import org.team1251.frc.robot.subsystems.Arm;
+import org.team1251.frc.robot.subsystems.CargoCollector;
+import org.team1251.frc.robot.subsystems.Climber;
 import org.team1251.frc.robot.subsystems.DriveBase;
 
 /**
@@ -14,15 +18,11 @@ import org.team1251.frc.robot.subsystems.DriveBase;
  */
 public class MotorTest extends Command {
 
-    /**
-     * The number of ticks to test each motor.
-     */
-    private final static int TEST_DURATION = 150; // 3 seconds
+    private static final double REST_PERIOD = .25;
 
-    /**
-     * The number of ticks that the test has been running for a given motor.
-     */
-    private int elapsedTestTicks = 0;
+    private boolean isResting = false;
+
+    private final Timer timer = new Timer();
 
     /**
      * The driveBase is the subsystem which provides access to the motors.
@@ -30,17 +30,44 @@ public class MotorTest extends Command {
     private final DriveBase driveBase;
 
     /**
-     * The motor which is currently being tested or `null` if no motor is being tested.
+     * The motor which is currently being tested.
      */
-    private DriveBase.Motor currentMotor = null;
+    private Motor currentMotor = Motor.NONE;
+
+    public enum Motor {
+        NONE(.3), // Add in a little extra bit of nothing at the start of each cycle. pause = testDuration + (rest * 2)
+        DRIVE_LEFT_TOP,
+        DRIVE_LEFT_BOTTOM_FRONT,
+        DRIVE_LEFT_BOTTOM_REAR,
+        DRIVE_RIGHT_TOP,
+        DRIVE_RIGHT_BOTTOM_FRONT,
+        DRIVE_RIGHT_BOTTOM_REAR,
+
+        LIFT_FRONT,
+        LIFT_REAR,
+        LIFT_DRIVE,
+
+        ARM,
+        CARGO_COLLECTOR,
+        ELEVATOR;
+
+        public final double testDuration;
+
+        Motor() {
+            this(.25);
+        }
+
+        Motor(double testDuration) {
+            this.testDuration = testDuration;
+        }
+    }
 
     /**
      * Creates an instance.
      *
      * @param driveBase The subsystem that provides access to the motors.
      */
-    public MotorTest(DriveBase driveBase) {
-
+    public MotorTest(DriveBase driveBase, Arm arm, CargoCollector collector, Climber climber) {
         // The drive base is a required subsystem.
         this.driveBase = driveBase;
         requires(driveBase);
@@ -49,28 +76,39 @@ public class MotorTest extends Command {
     /**
      * Resets the test.
      */
-    public void reset() {
-        elapsedTestTicks = 0;
-        currentMotor = null;
+    public void reset(Motor startingMotor) {
+        isResting = true;
+        currentMotor = startingMotor;
+        timer.reset();
+    }
+
+    @Override
+    protected void initialize() {
+        timer.start();
+    }
+
+    private void stopAllTests() {
+        // TODO: Stop all motor tests.
+        driveBase.testMotor(null);
     }
 
     @Override
     protected void end() {
-        driveBase.testMotor(null);
+        // stop but don't reset -- we'll pick up where we left off on next execution unless somebody
+        // explicitly calls reset() before then.
+        timer.stop();
+        stopAllTests();
     }
 
     @Override
     protected void execute() {
 
+
         // Keep track of how many ticks have passed while testing is active then combine it with the tick period to
         // derive a time-based measure of how much testing time has passed.
-        elapsedTestTicks++;
-        if (elapsedTestTicks >= TEST_DURATION) {
-            elapsedTestTicks = 0;
-            advanceMotor();
-        }
+        selectMotor();
 
-        driveBase.testMotor(currentMotor);
+        // TODO: Test appropriate motor.
     }
 
     @Override
@@ -84,43 +122,78 @@ public class MotorTest extends Command {
      *
      * After the last motor in the sequence, the motor is set to `null` providing a rest period.
      */
-    private void advanceMotor() {
+    private void selectMotor() {
 
-        // New motor, reset elapsed ticks.
-        this.elapsedTestTicks = 0;
+        // See if we are resting
+        if (isResting) {
+            if (!timer.hasPeriodPassed(REST_PERIOD)) {
+                return;
+            }
 
-        // Figure out which motor to run next.
-        if (currentMotor == null) {
-            // No current motor, select the first one in the sequence.
-            currentMotor = DriveBase.Motor.LEFT_TOP;
+            // rest time is over!
+            isResting = false;
+            timer.reset();
             return;
+        }
+
+        // If there is no motor selected or the test duration has elapsed for the selected motor, move to the next one.
+        if (currentMotor == null || timer.hasPeriodPassed(currentMotor.testDuration)) {
+            nextMotor();
+        }
+    }
+
+    private void nextMotor() {
+
+        // We always activate the rest period when advancing motors.
+        isResting = true;
+        timer.reset();
+
+        // Handle first motor.
+        if (currentMotor == null) {
+            currentMotor = Motor.NONE;
         }
 
         // There is a current motor, advance to the next one in the sequence.
         switch (currentMotor) {
-
-            case LEFT_TOP:
-                currentMotor = DriveBase.Motor.LEFT_BOTTOM_FRONT;
+            case NONE:
+                currentMotor = Motor.DRIVE_LEFT_TOP;
                 break;
-
-            case LEFT_BOTTOM_FRONT:
-                currentMotor = DriveBase.Motor.LEFT_BOTTOM_REAR;
+            case DRIVE_LEFT_TOP:
+                currentMotor = Motor.DRIVE_LEFT_BOTTOM_FRONT;
                 break;
-
-            case LEFT_BOTTOM_REAR:
-                currentMotor = DriveBase.Motor.RIGHT_TOP;
+            case DRIVE_LEFT_BOTTOM_FRONT:
+                currentMotor = Motor.DRIVE_LEFT_BOTTOM_REAR;
                 break;
-
-            case RIGHT_TOP:
-                currentMotor = DriveBase.Motor.RIGHT_BOTTOM_FRONT;
+            case DRIVE_LEFT_BOTTOM_REAR:
+                currentMotor = Motor.DRIVE_RIGHT_TOP;
                 break;
-
-            case RIGHT_BOTTOM_FRONT:
-                currentMotor = DriveBase.Motor.RIGHT_BOTTOM_REAR;
+            case DRIVE_RIGHT_TOP:
+                currentMotor = Motor.DRIVE_RIGHT_BOTTOM_FRONT;
                 break;
-
-            case RIGHT_BOTTOM_REAR:
-                currentMotor = null; // Rest period after we've ran all motors.
+            case DRIVE_RIGHT_BOTTOM_FRONT:
+                currentMotor = Motor.DRIVE_RIGHT_BOTTOM_REAR;
+                break;
+            case DRIVE_RIGHT_BOTTOM_REAR:
+                currentMotor = Motor.LIFT_FRONT;
+                break;
+            case LIFT_FRONT:
+                currentMotor = Motor.LIFT_REAR;
+                break;
+            case LIFT_REAR:
+                currentMotor = Motor.LIFT_DRIVE;
+                break;
+            case LIFT_DRIVE:
+                currentMotor = Motor.ELEVATOR;
+                break;
+            case ELEVATOR:
+                currentMotor = Motor.ARM;
+                break;
+            case ARM:
+                currentMotor = Motor.CARGO_COLLECTOR;
+                break;
+            case CARGO_COLLECTOR:
+                currentMotor = Motor.DRIVE_LEFT_TOP;
+                break;
         }
 
     }
